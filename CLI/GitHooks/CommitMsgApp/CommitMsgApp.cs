ef9bf792c7f02861e396af822a24e2cc6a61369b
EdwinToTheBatCave <159044353+EdwinToTheBatCave@users.noreply.github.com>
2024-04-09 13:22:13 +0200

    explain:Added logic to remove flags in commit message

--- CLI/GitHooks/CommitMsgApp/CommitMsgApp.cs
+++ CLI/GitHooks/CommitMsgApp/CommitMsgApp.cs
@@ -1,20 +1,20 @@
-﻿using Shared;
+﻿using System.Text.RegularExpressions;
+using Shared;
 
 namespace Cli.GitHooks
 {
-    public class CommitMsgApp 
+    public partial class CommitMsgApp 
     {
+        private static readonly Regex regex = FlagRegex();
         public static void Main(string[] args)
         {
             string commitMsgFile = args[0];
             CliFileHelper fileHelper = new(commitMsgFile);
-
-            
+            string contents = fileHelper.ReadFile();
+            if (regex.Matches(contents).Count > 0) fileHelper.UpdateFileContents(contents.Remove(0, "ctts-explain:".Length));
         }
 
-        private static void UpdateFileContent(string commitMsgFile, string content)
-        {
-            
-        }
+        [GeneratedRegex(@"^ctts-explain:")]
+        private static partial Regex FlagRegex();
     }
 }
