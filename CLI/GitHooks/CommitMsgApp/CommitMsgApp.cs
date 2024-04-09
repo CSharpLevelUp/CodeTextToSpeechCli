edd4d7325b34a17f2fbdcabe9461580d1de5e302
EdwinToTheBatCave <159044353+EdwinToTheBatCave@users.noreply.github.com>
2024-04-09 14:21:46 +0200

    Commit message flag capture test

--- CLI/GitHooks/CommitMsgApp/CommitMsgApp.cs
+++ CLI/GitHooks/CommitMsgApp/CommitMsgApp.cs
@@ -9,12 +9,18 @@ namespace Cli.GitHooks
         public static void Main(string[] args)
         {
             string commitMsgFile = args[0];
+            UpdateFlaggedCommitFile(commitMsgFile);
+        }
+
+        // Builds regex at compile time
+        [GeneratedRegex(@"^ctts-explain:")]
+        private static partial Regex FlagRegex();
+
+        public static void UpdateFlaggedCommitFile(string commitMsgFile)
+        {
             CliFileHelper fileHelper = new(commitMsgFile);
             string contents = fileHelper.ReadFile();
             if (regex.Matches(contents).Count > 0) fileHelper.UpdateFileContents(contents.Remove(0, "ctts-explain:".Length));
         }
-
-        [GeneratedRegex(@"^ctts-explain:")]
-        private static partial Regex FlagRegex();
     }
 }
---
+++ CLI/Tests/CommitMsgAppTests.cs
@@ -0,0 +1,20 @@
+﻿using Cli.GitHooks;
+using Shared;
+
+namespace Tests;
+
+[TestClass]
+public class CommitMsgAppTests
+{
+    private readonly string workingDir = "../../../Resources/";
+    [TestMethod]
+    public void TestUpdateFlaggedCommitFile()
+    {
+        string filePath = Path.Join([workingDir, "COMMIT_EDITMSG"]);
+        CliFileHelper fileHelper = new(filePath);
+        string oldCommitMsg = fileHelper.ReadFile();
+        CommitMsgApp.UpdateFlaggedCommitFile(filePath);
+        Assert.AreEqual(fileHelper.ReadFile(), "Added logic to remove flags in commit message");
+        fileHelper.UpdateFileContents(oldCommitMsg);
+    }
+}
