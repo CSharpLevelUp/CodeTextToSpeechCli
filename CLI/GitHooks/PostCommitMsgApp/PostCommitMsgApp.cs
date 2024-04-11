2b5a0159edc70e7b41d9b2ad8201c87c56467ce0
EdwinToTheBatCave <159044353+EdwinToTheBatCave@users.noreply.github.com>
2024-04-11 12:18:08 +0200

    Aligned post commit to openai class api

--- CLI/GitHooks/PostCommitMsgApp/PostCommitMsgApp.cs
+++ CLI/GitHooks/PostCommitMsgApp/PostCommitMsgApp.cs
@@ -1,23 +1,18 @@
 ﻿using Shared;
+using OpenaiSummarizer;
 
 namespace Cli.GitHooks
 {
     public class PostCommitMsgApp
     {
-        private static readonly GitWrapper gitWrapper = new(); 
         private static readonly CliFileHelper fileHelper = new(".");
         public static void Main(string[] args)
         {
             var flagSearch = fileHelper.SearchInLowestDirectory("CTTS_COMMIT_FLAG");
-            if (flagSearch is not null)
+            if (true)
             {
-                GitCommitFiles previousHashFiles = gitWrapper.GetCommitHashAndFiles();
-                foreach(string file in previousHashFiles.Files)
-                {
-                    string diff = gitWrapper.GetDiffForPreviousCommit(file);
-                    // Send diffs to open-ai
-                }
-                fileHelper.DeletePath("CTTS_COMMIT_FLAG");
+                Program.Main([new GitWrapper().GetDiffForPreviousCommit()]);
+                // fileHelper.DeletePath("CTTS_COMMIT_FLAG");
             }
         }
     }
