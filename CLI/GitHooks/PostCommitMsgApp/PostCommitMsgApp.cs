81858d7ff0bf5b9aed35871c01b29a78a4e86fc3
EdwinToTheBatCave <159044353+EdwinToTheBatCave@users.noreply.github.com>
2024-04-12 01:30:24 +0200

    Auth on  hook

--- CLI/GitHooks/PostCommitMsgApp/PostCommitMsgApp.cs
+++ CLI/GitHooks/PostCommitMsgApp/PostCommitMsgApp.cs
@@ -13,8 +13,8 @@ namespace Cli.GitHooks
         public static async Task Main(string[] args)
         {
             var flagSearch = fileHelper.SearchInLowestDirectory("CTTS_COMMIT_FLAG");
-            // if (flagSearch is not null)
-            if (true)
+            if (flagSearch is not null)
+            // if (true)
             {
                 TokenResponse accessAuth = null;
                 var authService = new AuthService();
@@ -35,15 +35,28 @@ namespace Cli.GitHooks
                     CliFileHelper fileHelper = new(path);
                     string diff = new GitWrapper().GetDiffForPreviousCommit().Replace('"', '\'');
                     string summary = OpenAIHelper.GetDiffSummary(diff, fileHelper.ReadFile());
-                    if (summary != null) BackendClient.SendCommitSummary(diff, summary);
+                    Console.WriteLine($"{summary}");
+                    OpenCommandPrompt(summary);
+                    if (summary != null) BackendClient.SendCommitSummary(diff, summary, accessAuth.access_token);
                     else throw new Exception("Error: Unable to fetch summary from OpenAI API.");
                 } catch(CliFileHelperException e)
                 {
                     CliFileHelper.CreateAppDirInLocalAppDataIfNotExist();
                     throw new CliCommandInvalidException("Register your open ai api key by running the set-openai-api-key --key=<openai api key>");
                 }
-                // fileHelper.DeletePath("CTTS_COMMIT_FLAG");
+                fileHelper.DeletePath("CTTS_COMMIT_FLAG");
             }
+        }
+
+        public static void OpenCommandPrompt(string summary)
+        {
+            System.Diagnostics.Process process = new System.Diagnostics.Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Normal;
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = $"echo \"{summary}\"";
+            process.StartInfo = startInfo;
+            process.Start();
         }
     }
 }
