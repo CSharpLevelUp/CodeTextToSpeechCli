cdd1eaa5a27b3f63132d4ad380dc3be39424657f
EdwinToTheBatCave <159044353+EdwinToTheBatCave@users.noreply.github.com>
2024-04-12 01:39:17 +0200

    removed dead code

--- CLI/OpenaiSummarizer/Program.cs
+++ CLI/OpenaiSummarizer/Program.cs
@@ -5,25 +5,6 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        string diffFile = args[0];
-        string access_token = args[1];
-        Console.WriteLine($"Your diff file is:\n{diffFile}\n");
-        Console.WriteLine($"access_token:\n{access_token}\n");
-        string apiKey = Environment.GetEnvironmentVariable("CLI_OPENAI_KEY");
 
-        Console.WriteLine("Calling Openai to summarize the diff file...");
-        string summary = OpenAIHelper.GetDiffSummary(diffFile, apiKey);
-
-        if (summary != null)
-        {
-            Console.WriteLine("Summary: \n" + summary + "\n");
-
-            Console.WriteLine("Calling backend application: /api/Git...");
-            BackendClient.SendCommitSummary(diffFile, summary);
-        }
-        else
-        {
-            Console.WriteLine("Error: Unable to fetch summary from OpenAI API.");
-        }
     }
 }
