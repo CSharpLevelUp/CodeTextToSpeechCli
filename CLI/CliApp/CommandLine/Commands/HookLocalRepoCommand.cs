195232d733a2f573fa21f074d69908c0360ef5b5
EdwinToTheBatCave <159044353+EdwinToTheBatCave@users.noreply.github.com>
2024-04-12 09:57:00 +0200

    Hook links

--- CLI/CliApp/CommandLine/Commands/HookLocalRepoCommand.cs
+++ CLI/CliApp/CommandLine/Commands/HookLocalRepoCommand.cs
@@ -20,8 +20,8 @@ namespace CliApp.CommandLine.Commands
             using(WebClient client = new())
             {
                 string[] fileUrls = [
-                    "https://github.com/CSharpLevelUp/CodeTextToSpeechCli/releases/download/v0.0.1-feature-cli-ci-cd.7/commit-msg.exe",
-                    "https://github.com/CSharpLevelUp/CodeTextToSpeechCli/releases/download/v0.0.1-feature-cli-ci-cd.7/post-commit.exe"
+                    "https://github.com/CSharpLevelUp/CodeTextToSpeechCli/releases/download/v0.0.1-develop.17/commit-msg.exe",
+                    "https://github.com/CSharpLevelUp/CodeTextToSpeechCli/releases/download/v0.0.1-develop.17/post-commit.exe"
                 ];
 
                 foreach(string fileUrl in fileUrls)
