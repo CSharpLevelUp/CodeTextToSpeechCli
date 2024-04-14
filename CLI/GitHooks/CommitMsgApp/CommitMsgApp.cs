5dcb33174e3f2446c37fda293e8bd273ea179fda
EdwinToTheBatCave <159044353+EdwinToTheBatCave@users.noreply.github.com>
2024-04-14 19:31:45 +0200

    Save access token

--- CLI/GitHooks/CommitMsgApp/CommitMsgApp.cs
+++ CLI/GitHooks/CommitMsgApp/CommitMsgApp.cs
@@ -1,9 +1,11 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Text.Json;
+using System.Text.RegularExpressions;
 using Cli.GitHooks.Services.AuthService;
 using CliApp.CommandLine.DataClasses;
 using CliApp.CommandLine.Exceptions;
 using OpenaiSummarizer;
 using Shared;
+using static OpenaiSummarizer.BackendClient;
 
 namespace Cli.GitHooks
 {
@@ -43,34 +45,66 @@ namespace Cli.GitHooks
             return fileHelper.CreateInPath("CTTS_FLAGGED_COMMIT");
         }
 
-        public static async Task AuthUser()
+        public static async Task SaveAccesssToken()
         {
-            TokenResponse accessAuth = null;
-            var authService = new AuthService();
-            if (authService.GetAccessToken() == null)
-            {
-                accessAuth = await authService.GetAccessTokenAsync("https://dev-01dgwqpxxjssj0wd.us.auth0.com/oauth/token");
-            }
-            else
-            {
-                accessAuth = authService.GetAccessToken();
-            }
-
+            string diff = new GitWrapper().GetDiffForPreviousCommit().Replace('"', '\'');
+            CliFileHelper fileHelper = new(".");
+            bool isNewAuth = false;
             try 
             {
-                string path = Path.Join([Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData, Environment.SpecialFolderOption.Create), "CodeTextToSpeech", "openai-key"]);
-                CliFileHelper fileHelper = new(path);
-                string diff = new GitWrapper().GetDiffForPreviousCommit().Replace('"', '\'');
-                Console.WriteLine($"diff:\n {diff}");
-                string summary = OpenAIHelper.GetDiffSummary(diff, fileHelper.ReadFile());
-                Console.WriteLine($"summary:\n {summary}");
+                TokenResponse accessAuth;
+                if (fileHelper.SearchInLowestDirectory("user-auth-token.json") is not null)
+                {                    
+                    accessAuth = JsonSerializer.Deserialize<TokenResponse>(fileHelper.ReadFile());
+                } else {
+                    accessAuth = await AuthUser();
+                    SaveTokenFile(accessAuth);
+                    isNewAuth = true;
+                }
+                string summary = GetSummary(diff);
                 if (summary != null) BackendClient.SendCommitSummary(diff, summary, accessAuth.access_token);
                 else throw new Exception("Error: Unable to fetch summary from OpenAI API.");
-            } catch(CliFileHelperException e)
+            } catch(CliFileHelperException)
             {
                 CliFileHelper.CreateAppDirInLocalAppDataIfNotExist();
                 throw new CliCommandInvalidException("Register your open ai api key by running the set-openai-api-key --key=<openai api key>");
+            } catch(BackendClientException)
+            {
+                if (isNewAuth) throw new Exception("Can't connect to Backend");
+                var accessAuth = await AuthUser();
+                SaveTokenFile(accessAuth);
+                string summary = GetSummary(diff);
+                if (summary != null) BackendClient.SendCommitSummary(diff, summary, accessAuth.access_token);
+                else throw new Exception("Error: Unable to fetch summary from OpenAI API.");
             }
+        }
+        public static async Task<TokenResponse> AuthUser()
+        {
+            var authService = new AuthService();
+            TokenResponse accessAuth = (authService.GetAccessToken() == null) ?
+                await authService.GetAccessTokenAsync("https://dev-01dgwqpxxjssj0wd.us.auth0.com/oauth/token"):
+                authService.GetAccessToken();
+            Console.WriteLine($"Access Token: {accessAuth.access_token}");
+            Console.WriteLine($"ID Token: {accessAuth.ToString()}");
+            return accessAuth;
+        }
+
+        public static string GetSummary(string diff)
+        {
+            string path = Path.Join([Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData, Environment.SpecialFolderOption.Create), "CodeTextToSpeech", "openai-key"]);
+            CliFileHelper fileHelper = new(path);
+            string summary = OpenAIHelper.GetDiffSummary(diff, fileHelper.ReadFile());
+            return summary;
+        }
+
+        public static void SaveTokenFile(TokenResponse accessAuth)
+        {
+            string tokenPath = Path.Join([Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData, Environment.SpecialFolderOption.Create), "CodeTextToSpeech"]);
+            CliFileHelper fileHelper = new(tokenPath);
+            var tokenFilePath = Path.Join([tokenPath, "user-auth-token.json"]);
+            if (!File.Exists(tokenFilePath))
+                CliFileHelper.CreateInLocalAppData(Path.Join(["CodeTextToSpeech", "user-auth-token.json"]));
+            new CliFileHelper(tokenFilePath).UpdateFileContents(JsonSerializer.Serialize<TokenResponse>(accessAuth));
         }
     }
 }
